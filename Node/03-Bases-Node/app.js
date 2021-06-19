4a84331458d6acb56596580de6f0818e1a5171d1
Stephanie <s.castillo.laiza@accenture.com>
2021-06-18 22:29:44 -0300

    "Se realiza la descarga de package.js para la utilizacion del package de colores y tambien se documenta que en package.js se puede agregar scripts y correr el programa utilizando 'npm run nombre' donde nombre es como queremos hacer referencia a dicho scripts en nuestro caso si quisieramos imprimir la tabla del 3 pondriamos por ejemplo: "base3": "node app --base=3" y lo ejecutariamos simplemente como base3."

--- Node/03-Bases-Node/app.js
+++ Node/03-Bases-Node/app.js
@@ -1,9 +1,11 @@
 console.clear();
 
-const FS = require('fs');
 const {CREAR_ARCHIVO_TABLA} = require('./resources/multiplicar');
-const BASE = 10;
 
-CREAR_ARCHIVO_TABLA(BASE)
+const [,,arg3 = 'base=5'] = process.argv;
+
+const [,base = 5] = arg3.split('=');
+
+CREAR_ARCHIVO_TABLA(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err));
