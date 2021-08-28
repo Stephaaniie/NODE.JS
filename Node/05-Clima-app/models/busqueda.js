ab2baaa80e3dde20ccdb083f7273e42a3425f968
Stephanie <s.castillo.laiza@accenture.com>
2021-08-27 21:52:03 -0300

    Se agrega solicitud https para la solicitud de climas

--- Node/05-Clima-app/models/busqueda.js
+++ Node/05-Clima-app/models/busqueda.js
@@ -10,7 +10,8 @@ class Busqueda {
 
     async ciudad(lugar = ''){
 
-        console.log(lugar);
+       // console.log(lugar);
+        const RESP = await AXIOS.get('https://api.mapbox.com/geocoding/v5/mapbox.places/-58.3778060483437%2C-34.60086615658818.json?access_token=pk.eyJ1Ijoic2NlbGkiLCJhIjoiY2tzdjIycGJyMWxiaTJvbzM1dmlpMGV3diJ9.GXGFAfNRE7Jr5V9IetYdaQ');
         return[];
     }
 }
